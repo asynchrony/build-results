9fe5a7bb879839bf8c4093c98807f427599addf6
brian.smith <brian.smith@asolutions.com>
2012-09-06 10:22:40 -0500

    Added print for single test with all failures - Brian

--- BuildUtils/PrintCommonFailures.cs
+++ BuildUtils/PrintCommonFailures.cs
@@ -9,21 +9,28 @@ namespace BuildUtils
 {
     public class PrintCommonFailures
     {
-//        [Ignore]
-        [Test]
-        public void TestName()
+        public static void Main(string[] args)
         {
-            print();
+            PrintAllTestFailures();
         }
 
-        public void print()
+        [Test]
+        public void TestAllFailures()
         {
-            var buildDirName = @"Z:\hudson_home\jobs\Branch_Runner_2\builds";
-            var directories = Directory.GetDirectories(buildDirName);
-            
-//            var testResults = new Dictionary<string, int>();
+            PrintAllTestFailures();
+        }
 
-            var tests = new HashSet<TestResult>();
+        [Test]
+        public void TestAllSingleFailure()
+        {
+            PrintSingleTest();
+        }
+
+        public static void PrintSingleTest()
+        {
+            var directories = GetBuildDirectories();
+
+            var tests = new List<TestResult>();
 
             foreach (var dir in directories)
             {
@@ -31,60 +38,132 @@ namespace BuildUtils
                 if (files.Any())
                 {
                     var fileName = files[0];
-                    XPathDocument doc = new XPathDocument(fileName);
-                    XPathNavigator nav = doc.CreateNavigator();
-                    XPathExpression expr = nav.Compile("//case[errorStackTrace]");
-                    XPathNodeIterator iterator = nav.Select(expr);
-                    
+                    var iterator = GetTestCasesWithErrors("UserAcceptanceTests.Features.ProductFeature","RefreshOnProductAfterAddingCommentShouldNOTResultInErrorBugFix",fileName);
+
                     while (iterator.MoveNext())
                     {
-                        XPathNavigator testCaseNavigator = iterator.Current;
+                        var failingTest = GetFailingTestName(iterator);
 
-                        XPathNavigator classNameNav = testCaseNavigator.SelectSingleNode("className/text()");
-                        XPathNavigator testNameNav = testCaseNavigator.SelectSingleNode("testName/text()");
+                        var testResult = new TestResult {BuildName = GetBuildName(dir),TestName = failingTest};
 
-                        var failingTest = classNameNav + "." + testNameNav;
-
-                        var testResult = GetTestResult(failingTest,tests);
-
-                        TestResult newTestResult;
-                        if (testResult != null)
-                        {
-                            tests.Remove(testResult);
-                           newTestResult =  new TestResult{BuildName = testResult.BuildName,TestName = testResult.TestName,TestFailures = testResult.TestFailures++};
-                           newTestResult.TestFailures = testResult.TestFailures++;
-                           
-                        } else
-                        {
-                           newTestResult = new TestResult{BuildName = dir.Substring(dir.LastIndexOf("\\")+1),TestName = failingTest,TestFailures = 1};
-                        }
-                        tests.Add(newTestResult);
-                            
+                        tests.Add(testResult);
                     }
                 }
  
             }
-            foreach (var failingTest in tests.ToList().OrderBy(x=>x.TestFailures).Reverse())
+            foreach (var failingTest in tests.ToList().OrderBy(x=>x.BuildName).Reverse())
             {
                 Console.WriteLine(failingTest);
             }
         }
 
-        private TestResult GetTestResult(string testName, IEnumerable<TestResult> allTests )
+        public static void PrintAllTestFailures()
+        {
+            var directories = GetBuildDirectories();
+
+            var tests = new HashSet<AggregateTestResult>();
+
+            var totalBuilds = 0;
+            var days = 1;
+
+            foreach (var dir in directories.Where(x=>Directory.GetCreationTime(x) > DateTime.Now.AddDays(-1 * days)))
+            {
+                totalBuilds++;
+                var files = Directory.GetFiles(dir, "junitResult.xml");
+                if (files.Any())
+                {
+                    var fileName = files[0];
+                    var iterator = GetTestCasesWithErrors(fileName);
+
+                    while (iterator.MoveNext())
+                    {
+                        var failingTest = GetFailingTestName(iterator);
+
+                        var testResult = GetTestResult(failingTest, tests);
+
+                        UpdateResults(failingTest, dir, tests, testResult);
+                    }
+                }
+            }
+            foreach (var failingTest in tests.ToList().OrderBy(x=>x.FailureCount).Reverse())
+            {
+                Console.WriteLine(failingTest);
+            }
+            Console.WriteLine("Tests performed during last: " + days + " days");
+            Console.WriteLine("Total Builds performed during test run: " + totalBuilds);
+        }
+
+        private static void UpdateResults(string failingTest, string dir, HashSet<AggregateTestResult> tests, AggregateTestResult aggregateTestResult)
+        {
+            AggregateTestResult newAggregateTestResult;
+            if (aggregateTestResult != null)
+            {
+                tests.Remove(aggregateTestResult);
+                newAggregateTestResult = new AggregateTestResult {BuildName = aggregateTestResult.BuildName, TestName = aggregateTestResult.TestName, FailureCount = ++aggregateTestResult.FailureCount};
+            }
+            else
+            {
+                newAggregateTestResult = new AggregateTestResult {BuildName = GetBuildName(dir), TestName = failingTest, FailureCount = 1};
+            }
+            tests.Add(newAggregateTestResult);
+        }
+
+        private static string GetBuildName(string dir)
+        {
+            return dir.Substring(dir.LastIndexOf("\\") + 1);
+        }
+
+        private static string GetFailingTestName(XPathNodeIterator iterator)
+        {
+            XPathNavigator testCaseNavigator = iterator.Current;
+
+            XPathNavigator classNameNav = testCaseNavigator.SelectSingleNode("className/text()");
+            XPathNavigator testNameNav = testCaseNavigator.SelectSingleNode("testName/text()");
+
+            var failingTest = classNameNav + "." + testNameNav;
+            return failingTest;
+        }
+
+        private static XPathNodeIterator GetTestCasesWithErrors(string fileName)
+        {
+            XPathDocument doc = new XPathDocument(fileName);
+            XPathNavigator nav = doc.CreateNavigator();
+            XPathExpression expr = nav.Compile("//case[errorStackTrace]");
+            XPathNodeIterator iterator = nav.Select(expr);
+            return iterator;
+        }
+
+        private static XPathNodeIterator GetTestCasesWithErrors(string className, string testName, string fileName)
+        {
+            XPathDocument doc = new XPathDocument(fileName);
+            XPathNavigator nav = doc.CreateNavigator();
+            XPathExpression expr = nav.Compile("//case[errorStackTrace and className='" +className+"' and testName='" +testName+"']");
+            XPathNodeIterator iterator = nav.Select(expr);
+            return iterator;
+        }
+
+        private static string[] GetBuildDirectories()
+        {
+            var buildDirName = @"Z:\hudson_home\jobs\Branch_Runner_2\builds";
+            var directories = Directory.GetDirectories(buildDirName);
+            return directories;
+        }
+
+        private static AggregateTestResult GetTestResult(string testName, IEnumerable<AggregateTestResult> allTests )
         {
             return allTests.ToList().SingleOrDefault(x => x.TestName == testName);
         }
     }
 
-    class TestResult
+    internal class AggregateTestResult
     {
         public string BuildName { get; set; }
         public string TestName { get; set; }
-        public int TestFailures { get; set; }
+        public int FailureCount { get; set; }
 
 
 
-        public bool Equals(TestResult other)
+        public bool Equals(AggregateTestResult other)
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
@@ -95,8 +174,8 @@ namespace BuildUtils
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != typeof (TestResult)) return false;
-            return Equals((TestResult) obj);
+            if (obj.GetType() != typeof (AggregateTestResult)) return false;
+            return Equals((AggregateTestResult) obj);
         }
 
         public override int GetHashCode()
@@ -106,7 +185,17 @@ namespace BuildUtils
 
         public override string ToString()
         {
-            return TestFailures + " : " + BuildName + " - " + TestName;
+            return String.Join(";", new[] {FailureCount + "", BuildName, TestName});
         }
+    }
+
+    internal class TestResult
+    {
+        public string TestName { get; set; }
+        public string BuildName { get; set; }
+        public override string ToString()
+        {
+            return String.Join(";", new[] {BuildName, TestName});
+        }        
     }
 }
